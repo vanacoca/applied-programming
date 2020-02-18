fa5b77e2a325971b870818bc100f826cdfc02eff
vanacoca <catherine.vanacore@kellogg.northwestern.edu>
2020-02-18 02:23:12 +0000

    Week 6

--- dice.rb
+++ dice.rb
@@ -4,12 +4,15 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-die1 = rand(1..6)
-die2 = rand(1..6)
-total = die1 + die2
 
-puts "#{die1} #{die2}"
-puts "The total is #{total}"
+@die1 = rand(1..6)
+@die2 = rand(1..6)
+view "dice"
+
+ #"<h1>#{die1} #{die2}</h1>"
+
+ 
+
 end
 
 # get "/catherine" do
@@ -17,5 +20,5 @@ end
 # end
 
 #how to commit the repository
-#go to the fork, name, commit, and push (check in on this via Google, how do i commit from gitpod to github)
+#click the 3 dots and go to stage all changes, go to fork, name, hit check mark tocl commit, and push (check in on this via Google, how do i commit from gitpod to github)
 
