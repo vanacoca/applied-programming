5dfab54fcf60dbb176046b07d2a8b364183418c5
vanacoca <catherine.vanacore@kellogg.northwestern.edu>
2020-02-18 01:51:10 +0000

    Week 6

--- dice.rb
+++ dice.rb
@@ -4,5 +4,18 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "Hello"
+die1 = rand(1..6)
+die2 = rand(1..6)
+total = die1 + die2
+
+puts "#{die1} #{die2}"
+puts "The total is #{total}"
 end
+
+# get "/catherine" do
+#   "<h1>It's me</html>"
+# end
+
+#how to commit the repository
+#go to the fork, name, commit, and push (check in on this via Google, how do i commit from gitpod to github)
+
--- weather.rb
+++ weather.rb
@@ -1,7 +1,7 @@
 require "forecast_io"
 
 # configure the Dark Sky API with your API key
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "3bb1b33e89ef956ed282a1a3c66e4827"
 
 # do the heavy lifting, use Global Hub lat/long
 forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
@@ -9,6 +9,38 @@ forecast = ForecastIO.forecast(42.0574063,-87.6722787).to_hash
 # pp = pretty print
 # use instead of `puts` to make reading a hash a lot easier
 # e.g. `pp forecast`
+
+#Gives API data for the forecast:
+#puts forecast
+
+#For a more readable format of the hash:
+#pp forecast
+
+#On the darksky API documentation website, you can look more into what is in the hash
+
+#To get the current forecast in Evanston
+
+#current_temperature = forecast["currently"]["temperature"]
+#conditions = forecast["currently"]["summary"]
+#puts "In Evanston, it is currently #{current_temperature} and #{conditions}"
+
+# do a loop to get the forecast for the next 7 days
+
+#first, this shows us what is in the daily hash, there is a data array showing the data by day
+#puts forecast["daily"]
+
+#then, we look at the data array
+#puts forecast["daily"]["data"]
+
+#get temperature
+#high_temperature = forecast["daily"]["data"][0]["temperatureHigh"]
+#puts high_temperature
+
+#writing the actual loop
+
+for day in forecast["daily"]["data"]
+    puts "A high temperature of #{day["temperatureHigh"]} and #{day["summary"]}"
+end
 
 # Example output:
 #
