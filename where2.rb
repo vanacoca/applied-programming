7955fc18b00290d176a27bcfb5398a228e49c31a
vanacoca <catherine.vanacore@kellogg.northwestern.edu>
2020-02-18 02:41:58 +0000

    Week 6

--- where2.rb
+++ where2.rb
@@ -6,4 +6,9 @@ before { puts "Parameters: #{params}" }
 get "/" do
   # lat: ± 90
   # long: ± 180
+
+@lat = rand(-90.0...90.0)
+@long = rand(-180.0...180.0)
+@lat_long = "#{@lat}, #{@long}"
+  view "where2"
 end
